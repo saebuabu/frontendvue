eafcc07f5c0b136de8427f4f8c9541785a233229
jamirojaf <jamiro.jafar@gmail.com>
2024-03-08 14:35:52 +0100

    movies data extern bestand

---
+++ src/assets/data/movies.js
@@ -0,0 +1,41 @@
+const movies = [
+    {
+        "titel": "Nomadland",
+        "regisseur": "Chloé Zhao",
+        "acteurs": ["Frances McDormand", "David Strathairn", "Linda May"],
+        "genre": "Drama"
+    },
+    {
+        "titel": "The Trial of the Chicago 7",
+        "regisseur": "Aaron Sorkin",
+        "acteurs": ["Eddie Redmayne", "Sacha Baron Cohen", "Mark Rylance"],
+        "genre": "Drama"
+    },
+    {
+        "titel": "Minari",
+        "regisseur": "Lee Isaac Chung",
+        "acteurs": ["Steven Yeun", "Yeri Han", "Alan Kim"],
+        "genre": "Drama"
+    },
+    {
+        "titel": "Promising Young Woman",
+        "regisseur": "Emerald Fennell",
+        "acteurs": ["Carey Mulligan", "Bo Burnham", "Alison Brie"],
+        "genre": "Thriller"
+    },
+    {
+        "titel": "Sound of Metal",
+        "regisseur": "Darius Marder",
+        "acteurs": ["Riz Ahmed", "Olivia Cooke", "Paul Raci"],
+        "genre": "Drama"
+    },
+    {
+        "titel": "Mank",
+        "regisseur": "David Fincher",
+        "acteurs": ["Gary Oldman", "Amanda Seyfried", "Lily Collins"],
+        "genre": "Biografie"
+    }
+];
+
+export default movies;
+
