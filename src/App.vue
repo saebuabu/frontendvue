1e3c60e9f691fde783a2f7c9ff8080b830fcbb46
jamirojaf <jamiro.jafar@gmail.com>
2024-03-19 17:18:37 +0100

    less voorbeeld in App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,10 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
+  <h1>Welkom App met less</h1>
+  <p class="bordered">Dit is een paragraafje</p>
 </template>
 
 <script>
-
 export default {
   name: 'App',
   components: {
@@ -11,13 +12,25 @@ export default {
 }
 </script>
 
-<style>
+<style lang="less">
+.bordered {
+  border-top: dotted 1px black;
+  border-bottom: solid 2px black;
+}
+
+
+@hoofdkleur: #4CAF50;
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
+  color: @hoofdkleur;
   margin-top: 60px;
+  p {
+    .bordered();
+  }
 }
+
 </style>
