c0e89c8b70c1f86cce7938c609772bc147dcdc05
jamirojaf <jamiro.jafar@gmail.com>
2024-03-27 20:12:34 +0100

    navbar hamburgermenu gefixed door o.a. navbarView aangepast

--- src/views/NavbarView.vue
+++ src/views/NavbarView.vue
@@ -1,30 +1,33 @@
 <template>
   <nav class="navbar navbar-expand-lg navbar-light bg-light">
-    <a class="navbar-brand" href="/"><img src="https://picsum.photos/seed/picsum/75/75" alt="Random Logo">
-</a>
-    <button
-      class="navbar-toggler"
-      type="button"
-      data-toggle="collapse"
-      data-target="#navbarNav"
-      aria-controls="navbarNav"
-      aria-expanded="false"
-      aria-label="Toggle navigation"
-    >
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarNav">
-      <ul class="navbar-nav">
-        <li class="nav-item active">
-          <router-link class="nav-link sr-only" to="/">Home</router-link>
-        </li>
-        <li class="nav-item">
-          <router-link class="nav-link" to="/about">Over ons</router-link>
-        </li>
-        <li class="nav-item">
-          <router-link class="nav-link" to="/contact">Contact</router-link>
-        </li>
-      </ul>
+    <div class="container-fluid">
+      <a class="navbar-brand" href="/"
+        ><img src="https://picsum.photos/seed/picsum/75/75" alt="Random Logo" />
+      </a>
+      <button
+        class="navbar-toggler"
+        type="button"
+        data-bs-toggle="collapse"
+        data-bs-target="#navbarNav"
+        aria-controls="navbarNav"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav">
+          <li class="nav-item">
+            <router-link class="nav-link sr-only" to="/">Home</router-link>
+          </li>
+          <li class="nav-item">
+            <router-link class="nav-link" to="/about">Over ons</router-link>
+          </li>
+          <li class="nav-item">
+            <router-link class="nav-link" to="/contact">Contact</router-link>
+          </li>
+        </ul>
+      </div>
     </div>
   </nav>
 </template>
@@ -32,6 +35,6 @@
 <style>
 .navbar-brand img {
   border-radius: 50%;
-  margin: .5rem;
+  margin: 0.5rem;
 }
 </style>
