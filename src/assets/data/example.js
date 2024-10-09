0f451f81777fc30127f1979efa4808e8e5adee4e
saebuabu <a.saebu@kw1c.nl>
2024-10-09 15:24:24 +0200

    2 voorbeelden van json bestanden

---
+++ src/assets/data/example.js
@@ -0,0 +1,16 @@
+{
+    "location": {
+                    "province": "Brabant",
+                    "country": "Netherlands"
+               },
+    "current": {
+        "temperature": 16,
+        "feels_like": 15,
+        "weather": "Partly cloudy",
+        "humidity": 78,
+        "wind_speed": 12,
+        "wind_direction": "NW",
+        "visibility": 10,
+        "uv_index": 3
+        }
+}
---
+++ src/assets/data/oscars.js
@@ -0,0 +1,48 @@
+{
+    "awards": "Academy Awards",
+    "year": 2023,
+    "category": "Best Picture",
+    "winners": [
+      {
+        "title": "Everything Everywhere All at Once",
+        "year": 2022,
+        "directors": ["Daniel Kwan", "Daniel Scheinert"],
+        "genre": ["Adventure", "Comedy", "Drama"],
+        "oscars_won": 7,
+        "awards": [
+          "Best Picture",
+          "Best Director",
+          "Best Actress",
+          "Best Supporting Actor",
+          "Best Supporting Actress",
+          "Best Film Editing",
+          "Best Original Screenplay"
+        ]
+      },
+      {
+        "title": "The Whale",
+        "year": 2022,
+        "directors": ["Darren Aronofsky"],
+        "genre": ["Drama"],
+        "oscars_won": 2,
+        "awards": [
+          "Best Actor",
+          "Best Makeup and Hairstyling"
+        ]
+      },
+      {
+        "title": "All Quiet on the Western Front",
+        "year": 2022,
+        "directors": ["Edward Berger"],
+        "genre": ["Action", "Drama", "War"],
+        "oscars_won": 4,
+        "awards": [
+          "Best International Feature Film",
+          "Best Cinematography",
+          "Best Production Design",
+          "Best Original Score"
+        ]
+      }
+    ]
+  }
+  
